5d999c0f389ce0b7e525db3cea063e2e4bbff9f6
quangpv <quangpv.uit@gmail.com>
2022-05-18 17:58:57 +0700

    Update source

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,15 @@
-nohup python -m http.server 8001 &
+PYTHON_CODE="""
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+"""
+
+nohup python3 <<< "$PYTHON_CODE" & 
